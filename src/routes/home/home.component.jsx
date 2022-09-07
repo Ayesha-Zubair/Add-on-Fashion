78e3d6bc24b231457e87925cd5dec8916f6aa91a
Ayesha Zubair <zk6891745@gmail.com>
2022-09-07 13:41:01 +0530

    firebase and signIn

---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,38 @@
+import Directory from "../../components/directory/directory.component";
+
+const Home = () => {
+
+  const categories = [
+    {
+      "id": 1,
+      "title": "Jewellery",
+      "imageUrl": "https://images.unsplash.com/photo-1620656798579-1984d9e87df7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    },
+    {
+      "id": 2,
+      "title": "Handbags",
+      "imageUrl": "https://images.unsplash.com/photo-1559563458-527698bf5295?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    },
+    {
+      "id": 3,
+      "title": "Shades",
+      "imageUrl": "https://images.unsplash.com/photo-1582142366243-0b6a4aca0d99?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    },
+    {
+      "id": 4,
+      "title": "Footwear",
+      "imageUrl": "https://images.unsplash.com/photo-1574791418059-2e8d961ab4fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1189&q=80"
+    },
+    {
+      "id": 5,
+      "title": "Watches",
+      "imageUrl": "https://images.unsplash.com/photo-1604242692760-2f7b0c26856d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80"
+    }
+  ]
+
+  return (
+      <Directory categories = {categories} />
+  );
+}
+
+export default Home;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,46 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyByeLTnjMuiu0rHlopZGRh18LxaoPDQPL8",
+    authDomain: "add-on-fashion-db.firebaseapp.com",
+    projectId: "add-on-fashion-db",
+    storageBucket: "add-on-fashion-db.appspot.com",
+    messagingSenderId: "71520269509",
+    appId: "1:71520269509:web:34c51d1d172a27363eecac"
+  };
+  
+  // Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: "select_account"
+})
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    const userSnapshot = await getDoc(userDocRef);
+
+if(!userSnapshot.exists()){
+    const { displayName, email } = userAuth;
+    const createAt = new Date();
+    try{
+        await setDoc(userDocRef, {
+            displayName,
+            email,
+            createAt
+        });
+    }catch (error){
+        console.log('error creating the user', error.message);
+    }
+}
+
+return userDocRef;
+};
